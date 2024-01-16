9de2dee7209d270948dc5929643e3b4f0f8c614f
GaetanBaehr <gaetan.baehr@black-foundry.com>
2024-01-16 10:34:03 +0100

    factorized diagram

--- scripts/diagram.py
+++ scripts/diagram.py
@@ -2,6 +2,7 @@ import pathlib
 from collections import defaultdict
 from drawbot_skia.drawbot import *
 
+
 RED_COLOR = (1, 0, 0, 1)
 ORANGE_COLOR = (1, 0.5, 0, 1)
 YELLOW_COLOR = (1, 1, 0, 1)
@@ -9,7 +10,9 @@ BLUE_COLOR = (0, 0.5, 1, 1)
 GREEN_COLOR = (0, 1, 0.5, 1)
 
 
-def make_page(text_infos, number_of_days_untile_deadline, number_of_glyphs, ends_date, title):
+def make_page(
+    text_infos, number_of_days_untile_deadline, number_of_glyphs, ends_date, title
+):
     dates = []
     reds = []
     oranges = []
@@ -33,7 +36,6 @@ def make_page(text_infos, number_of_days_untile_deadline, number_of_glyphs, ends
     margin = 50
 
     if number_of_days > 1:
-
         reds_list = reds
         oranges_list = oranges
         yellows_list = yellows
@@ -49,29 +51,23 @@ def make_page(text_infos, number_of_days_untile_deadline, number_of_glyphs, ends
                 greens_list[-1],
             ]
         )
+
         xWidth = pageWidth - 2 * margin
         yHeight = pageHeight - 2 * margin
-
-        xstep = xWidth / (number_of_days_untile_deadline-1)
+        xstep = xWidth / (number_of_days_untile_deadline - 1)
         ystep = yHeight / int(number_of_glyphs)
 
         newPage(pageWidth, pageHeight)
 
+        # background and vertical / horizontal lines
         fill(0.1)
         rect(0, 0, pageWidth, pageHeight)
         translate(margin, margin)
         stroke(1)
         line((0, 0), (xWidth, 0))
         line((0, 0), (0, yHeight))
-        with savedState():
-            stroke(1)
-            strokeWidth(1)
-            line((-10, yHeight), (0, yHeight))
-            stroke(None)
-            fill(1)
-            font("Helvetica", 5)
-            text(str(number_of_glyphs), (-15, yHeight-1), align = "right")
 
+        # left vertical white line with glyphs count
         for i in range(6):
             y = (int(maximumValue) / 5) * i
             stroke(1)
@@ -86,171 +82,64 @@ def make_page(text_infos, number_of_days_untile_deadline, number_of_glyphs, ends
 
         strokeWidth(2)
 
-        pol = [
-            (
-                0 * xstep,
-                int(
-                    reds_list[0]
-                    + oranges_list[0]
-                    + yellows_list[0]
-                    + greens_list[0]
-                    + blues_list[0]
-                )
-                * ystep,
-            )
+        # draw colors polygons
+        all_lists = [
+            dict(list=reds_list, color=RED_COLOR),
+            dict(list=oranges_list, color=ORANGE_COLOR),
+            dict(list=yellows_list, color=YELLOW_COLOR),
+            dict(list=blues_list, color=BLUE_COLOR),
+            dict(list=greens_list, color=GREEN_COLOR),
         ]
-        fill(*RED_COLOR)
-        for i, red in enumerate(reds_list[1:]):
-            pol.append(
+
+        for ci, color_list_infos in enumerate(all_lists):
+            color_list = color_list_infos["list"]
+            v = 0
+            for _color_list in all_lists[ci:]:
+                v += _color_list["list"][0]
+
+            polygon_points = [
                 (
-                    (i + 1) * xstep,
-                    int(
-                        reds_list[i + 1]
-                        + oranges_list[i + 1]
-                        + yellows_list[i + 1]
-                        + greens_list[i + 1]
-                        + blues_list[i + 1]
-                    )
-                    * ystep,
+                    0 * xstep,
+                    v * ystep,
                 )
-            )
-        pol.append(((len(reds_list) - 1) * xstep, 0))
-        pol.append((0, 0))
-        polygon(*pol)
-        stroke(None)
+            ]
 
-        value = int(reds_list[i + 1])
-        if value:
-            font("Helvetica", 5)
-            text(
-                str(value),
-                (
-                    (i + 1) * xstep + 5,
-                    int(
-                        reds_list[i + 1]
-                        + oranges_list[i + 1]
-                        + yellows_list[i + 1]
-                        + greens_list[i + 1]
-                        + blues_list[i + 1]
+            for i, color in enumerate(color_list[1:]):
+                v = 0
+                for _color_list in all_lists[ci:]:
+                    v += _color_list["list"][i + 1]
+                polygon_points.append(
+                    (
+                        (i + 1) * xstep,
+                        v * ystep,
                     )
-                    * ystep,
-                ),
-                align="left",
-            )
-
-        pol = [
-            (
-                0 * xstep,
-                int(oranges_list[0] + yellows_list[0] + greens_list[0] + blues_list[0])
-                * ystep,
-            )
-        ]
-        fill(*ORANGE_COLOR)
-        for i, red in enumerate(oranges_list[1:]):
-            pol.append(
-                (
-                    (i + 1) * xstep,
-                    int(
-                        oranges_list[i + 1]
-                        + yellows_list[i + 1]
-                        + greens_list[i + 1]
-                        + blues_list[i + 1]
-                    )
-                    * ystep,
                 )
-            )
-        pol.append(((len(oranges_list) - 1) * xstep, 0))
-        pol.append((0, 0))
-        polygon(*pol)
-        stroke(None)
 
-        value = int(oranges_list[i + 1])
-        if value:
-            font("Helvetica", 5)
-            text(
-                str(value),
-                (
-                    (i + 1) * xstep + 5,
-                    int(
-                        oranges_list[i + 1]
-                        + yellows_list[i + 1]
-                        + greens_list[i + 1]
-                        + blues_list[i + 1]
-                    )
-                    * ystep + 4 * (value < 200),
-                ),
-                align="left",
-            )
+            polygon_points.append(((len(color_list) - 1) * xstep, 0))
+            polygon_points.append((0, 0))
 
-        pol = [(0 * xstep, int(yellows_list[0] + greens_list[0] + blues_list[0]) * ystep)]
-        fill(*YELLOW_COLOR)
-        for i, red in enumerate(yellows_list[1:]):
-            pol.append(
-                (
-                    (i + 1) * xstep,
-                    int(yellows_list[i + 1] + greens_list[i + 1] + blues_list[i + 1])
-                    * ystep,
+            color = color_list_infos["color"]
+            fill(*color)
+            polygon(*polygon_points)
+
+            value = int(color_list[i + 1])
+            if value:
+                font("Helvetica", 7)
+                text(
+                    str(value),
+                    (
+                        (i + 1) * xstep + 5,
+                        v * ystep,
+                    ),
+                    align="left",
                 )
-            )
-        pol.append(((len(yellows_list) - 1) * xstep, 0))
-        pol.append((0, 0))
-        polygon(*pol)
-        stroke(None)
 
-        value = int(yellows_list[i + 1])
-        if value:
-            font("Helvetica", 5)
-            text(
-                str(value),
-                (
-                    (i + 1) * xstep + 5,
-                    int(yellows_list[i + 1] + greens_list[i + 1] + blues_list[i + 1])
-                    * ystep + 4 * (value < 200),
-                ),
-                align="left",
-            )
-
-        pol = [(0 * xstep, int(greens_list[0] + blues_list[0]) * ystep)]
-        fill(*BLUE_COLOR)
-        for i, red in enumerate(blues_list[1:]):
-            pol.append(
-                ((i + 1) * xstep, int(greens_list[i + 1] + blues_list[i + 1]) * ystep)
-            )
-        pol.append(((len(blues_list) - 1) * xstep, 0))
-        pol.append((0, 0))
-        polygon(*pol)
-        stroke(None)
-
-        value = int(blues_list[i + 1])
-        if value:
-            font("Helvetica", 5)
-            text(
-                str(value),
-                ((i + 1) * xstep + 5, int(greens_list[i + 1] + blues_list[i + 1]) * ystep + 4 * (value < 200)),
-                align="left",
-            )
-
-        pol = [(0 * xstep, int(greens_list[0]) * ystep)]
-        fill(*GREEN_COLOR)
-        for i, red in enumerate(greens_list[1:]):
-            pol.append(((i + 1) * xstep, int(greens_list[i + 1]) * ystep))
-        pol.append(((len(greens_list) - 1) * xstep, 0))
-        pol.append((0, 0))
-        polygon(*pol)
-        stroke(None)
-        # fill(1)
-        value = int(greens_list[i + 1])
-        if value:
-            font("Helvetica", 5)
-            text(
-                str(value),
-                ((i + 1) * xstep + 5, int(greens_list[i + 1]) * ystep ),
-                align="left",
-            )
-
+        # draw diagonal line
         stroke(1)
+        strokeWidth(1)
         line((0, 0), (xWidth, yHeight))
 
+        # draw deadline date
         with savedState():
             stroke(None)
             fill(1)
@@ -258,11 +147,11 @@ def make_page(text_infos, number_of_days_untile_deadline, number_of_glyphs, ends
             with savedState():
                 rotate(45, (xWidth, -20))
                 text(ends_date, (xWidth, -20), align="center")
-            # restore()
             stroke(1)
             strokeWidth(1)
             line((xWidth, 0), (xWidth, -10))
 
+        # draw dates
         for i, date in enumerate(dates):
             j = 5
             if number_of_days > j:
@@ -275,15 +164,13 @@ def make_page(text_infos, number_of_days_untile_deadline, number_of_glyphs, ends
             with savedState():
                 rotate(45, (i * xstep, -20))
                 text(date, (i * xstep, -20), align="center")
-            # restore()
             stroke(1)
             strokeWidth(1)
             line((i * xstep, 0), (i * xstep, -10))
 
+        # draw Title
         fontSize(16)
         text(title, (xWidth * 0.5, yHeight + 20), align="center")
-
-
 
 
 if __name__ == "__main__":
@@ -295,14 +182,22 @@ if __name__ == "__main__":
     projects_names = ["notoserifcjkjp", "notosanscjksc"]
     for project_name in projects_names:
         newDrawing()
-        colors_evolution_path = reportDir / f"{project_name}_evolution_character_glyphs.csv"
+        colors_evolution_path = (
+            reportDir / f"{project_name}_evolution_character_glyphs.csv"
+        )
         with open(colors_evolution_path, "r", encoding="utf-8") as file:
             colorEvolutionCGText = file.readlines()
 
         # get number of glyphs from last csv line
         last_line = colorEvolutionCGText[-1].strip().split(",")
         number_of_glyphs = sum([int(x) for x in last_line[1:]])
-        
-        make_page(colorEvolutionCGText, number_of_days_untile_deadline = 62, number_of_glyphs = number_of_glyphs, ends_date = "2024-03-11", title = "CG")
+
+        make_page(
+            colorEvolutionCGText,
+            number_of_days_untile_deadline=62,
+            number_of_glyphs=number_of_glyphs,
+            ends_date="2024-03-11",
+            title=f"{project_name} CG",
+        )
 
         saveImage(reportDir / f"{project_name}-progress.pdf")
