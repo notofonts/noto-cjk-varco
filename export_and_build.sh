3c9100f66e0df34e2585c0682a824871548748ae
Just van Rossum <justvanrossum@gmail.com>
2024-03-20 11:28:37 +0100

    Fiddle with folder names

--- export_and_build.sh
+++ export_and_build.sh
@@ -7,7 +7,7 @@ set -x  # echo commands
 mkdir -p build
 mkdir -p build/fontra
 mkdir -p build/ufo
-mkdir -p build/glyf-1
+mkdir -p build/ttf-varco
 mkdir -p build/ttf
 
 fontra-copy notosanscjksc.rcjk build/fontra/notosanscjksc.fontra
@@ -16,8 +16,8 @@ fontra-copy notoserifcjkjp.rcjk build/fontra/notoserifcjkjp.fontra
 fontra-copy notosanscjksc.rcjk build/ufo/notosanscjksc.designspace
 fontra-copy notoserifcjkjp.rcjk build/ufo/notoserifcjkjp.designspace
 
-fontra-compile notosanscjksc.rcjk build/glyf-1/notosanscjksc-glyf1.ttf
-fontra-compile notoserifcjkjp.rcjk build/glyf-1/notoserifcjkjp-glyf1.ttf
+fontra-compile notosanscjksc.rcjk build/ttf-varco/notosanscjksc-glyf1.ttf
+fontra-compile notoserifcjkjp.rcjk build/ttf-varco/notoserifcjkjp-glyf1.ttf
 
 fontra-workflow notosanscjksc.yaml --output-dir build/ttf
 fontra-workflow notoserifcjkjp.yaml --output-dir build/ttf
