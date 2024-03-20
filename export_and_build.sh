6b3bc8fd72e28c7c70e564e0d6a5fa083f4d32f7
Just van Rossum <justvanrossum@gmail.com>
2024-03-20 11:21:46 +0100

    Add glyph subset file and workflow file

--- export_and_build.sh
+++ export_and_build.sh
@@ -7,7 +7,8 @@ set -x  # echo commands
 mkdir -p build
 mkdir -p build/fontra
 mkdir -p build/ufo
-mkdir -p build/fonts
+mkdir -p build/glyf-1
+mkdir -p build/ttf
 
 fontra-copy notosanscjksc.rcjk build/fontra/notosanscjksc.fontra
 fontra-copy notoserifcjkjp.rcjk build/fontra/notoserifcjkjp.fontra
@@ -15,5 +16,8 @@ fontra-copy notoserifcjkjp.rcjk build/fontra/notoserifcjkjp.fontra
 fontra-copy notosanscjksc.rcjk build/ufo/notosanscjksc.designspace
 fontra-copy notoserifcjkjp.rcjk build/ufo/notoserifcjkjp.designspace
 
-fontra-compile notosanscjksc.rcjk build/fonts/notosanscjksc-glyf1.ttf
-fontra-compile notoserifcjkjp.rcjk build/fonts/notoserifcjkjp-glyf1.ttf
+fontra-compile notosanscjksc.rcjk build/glyf-1/notosanscjksc-glyf1.ttf
+fontra-compile notoserifcjkjp.rcjk build/glyf-1/notoserifcjkjp-glyf1.ttf
+
+# fontra-workflow notosanscjksc.yaml --output-dir build/ttf
+fontra-workflow notoserifcjkjp.yaml --output-dir build/ttf
