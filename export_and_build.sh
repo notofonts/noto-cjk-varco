70fc9ecc30d2c5129f2073b102d0f8b811b9b2c0
Just van Rossum <justvanrossum@gmail.com>
2024-07-16 08:44:59 +0200

    Export two artifacts: sources and fonts

--- export_and_build.sh
+++ export_and_build.sh
@@ -5,18 +5,18 @@ set -x  # echo commands
 
 
 mkdir -p build
-mkdir -p build/fontra
-mkdir -p build/ufo
-mkdir -p build/otf-varc
-mkdir -p build/ttf-varc
-mkdir -p build/otf
-mkdir -p build/ttf
+mkdir -p build/sources/fontra
+mkdir -p build/sources/ufo
+mkdir -p build/fonts/otf-varc
+mkdir -p build/fonts/ttf-varc
+mkdir -p build/fonts/otf
+mkdir -p build/fonts/ttf
 
-fontra-copy notosanscjksc.rcjk build/fontra/notosanscjksc.fontra
-fontra-copy notoserifcjkjp.rcjk build/fontra/notoserifcjkjp.fontra
+fontra-copy notosanscjksc.rcjk build/sources/fontra/notosanscjksc.fontra
+fontra-copy notoserifcjkjp.rcjk build/sources/fontra/notoserifcjkjp.fontra
 
-fontra-copy notosanscjksc.rcjk build/ufo/notosanscjksc.designspace
-fontra-copy notoserifcjkjp.rcjk build/ufo/notoserifcjkjp.designspace
+fontra-copy notosanscjksc.rcjk build/sources/ufo/notosanscjksc.designspace
+fontra-copy notoserifcjkjp.rcjk build/sources/ufo/notoserifcjkjp.designspace
 
-fontra-workflow notosanscjksc.yaml --output-dir build --continue-on-error
-fontra-workflow notoserifcjkjp.yaml --output-dir build --continue-on-error
+fontra-workflow notosanscjksc.yaml --output-dir build/fonts --continue-on-error
+fontra-workflow notoserifcjkjp.yaml --output-dir build/fonts --continue-on-error
